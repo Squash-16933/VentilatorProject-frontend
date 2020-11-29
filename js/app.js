d489e587033788bd84b7837b3184637df5557a63
WriterArtistCoder <tinystripz@outlook.com>
2020-11-29 13:46:59 -0800

    Fixed client-server communication

--- js/app.js
+++ js/app.js
@@ -4,32 +4,45 @@ class Protocol {
      * @param {String} type Message type
      * @param {*} [data] Data to send (if there is data)
      */
-    send(type, data=null) {
-        var self = this
+    send(type, data=null) { // Should I make this asynchronous?
+        var msg = {
+            type,
+            version: this.version,
+            request: this.request,
+            timestamp: Math.floor(new Date().getTime()),
+            data
+        }
 
-        console.log({
-            type,
-            version: self.version,
-            request: self.request,
-            timestamp: Math.floor(new Date().getTime()),
-            data
-        })
-        
-        // Should I make this asynchronous?
-        this.socket.send(JSON.stringify({
-            type,
-            version: self.version,
-            request: self.request,
-            timestamp: Math.floor(new Date().getTime()),
-            data
-        }))
-        self.request++
+        this.socket.send(JSON.stringify(msg))
+        this.request++
+
+        console.log('Sent:')
+        console.log(msg)
+        console.log()
     }
 
-    recieve(event) {
+    /**
+     * This listener triggers in response to the server sending its first message.
+     */
+    onReady() {
+        protocol.send('powerOn')
+    }
+
+    /**
+     * This listener triggers in response to recieval of a message.
+     * @param {MessageEvent} event 
+     */
+    onRecieve(event) { // If you put send() in this, it gets stuck in a feedback loop
         try {
             var data = JSON.parse(event.data)
+            console.log('Recieved:')
             console.log(data)
+            console.log()
+
+            if (!this.ready) {
+                this.ready = true
+                protocol.send('powerOn') // TODO Replace with this.onReady()
+            }
         } catch (e) { // If it's not in JSON format
             if (e instanceof SyntaxError) {
                 console.log(event.data)
@@ -41,8 +54,9 @@ class Protocol {
         this.version = '-1'
         this.socket  = new WebSocket('ws://localhost:3001');
         this.request = 0
+        this.ready   = false // True if ready for sending messages
 
-        this.socket.onmessage = this.recieve
+        this.socket.onmessage = this.onRecieve
     }
 }
 
